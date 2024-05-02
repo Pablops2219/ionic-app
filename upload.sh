d1af0371286ed9354e4f8c05303d6728194afaf3
Pablops <141527850+Pablops2219@users.noreply.github.com>
2024-05-02 08:54:52 +0200

    Commit automático: Thu May  2 08:54:52     2024

--- upload.sh
+++ upload.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+# Definir colores
+azul='\033[0;34m'
+normal='\033[0m'
+
 # Realizar un pull para obtener los cambios más recientes del repositorio remoto
-echo "Realizando un git pull..."
+echo -e "${azul}Realizando un git pull...${normal}"
 if ! git pull origin main; then
-    echo "Error: No se pudo realizar el git pull. Verifica tu conexión a Internet y el estado del repositorio remoto."
+    echo -e "${azul}Error: No se pudo realizar el git pull. Verifica tu conexión a Internet y el estado del repositorio remoto.${normal}"
     exit 1
 fi
 
 # Agregar todos los archivos al área de staging
-echo "Agregando archivos al área de staging..."
+echo -e "${azul}Agregando archivos al área de staging...${normal}"
 git add .
 
 # Hacer commit con un mensaje descriptivo
-echo "Haciendo commit de los cambios..."
+echo -e "${azul}Haciendo commit de los cambios...${normal}"
 git commit -m "Commit automático: $(date)"
 
 # Subir los cambios al repositorio remoto
-echo "Subiendo cambios al repositorio remoto..."
-if ! git push origin main; then
-    echo "Error: No se pudo realizar el git push. Verifica tu conexión a Internet y los permisos de tu cuenta de GitHub."
+echo -e "${azul}Subiendo cambios al repositorio remoto...${normal}"
+if ! git push; then
+    echo -e "${azul}Error: No se pudo realizar el git push. Verifica tu conexión a Internet y los permisos de tu cuenta de GitHub.${normal}"
     exit 1
 fi
 
-echo "Operaciones completadas exitosamente."
+echo -e "${azul}Operaciones completadas exitosamente.${normal}"
 
 # Esperar a que el usuario presione Enter para cerrar la consola
-read -p "Presiona Enter para salir..."
+read -p "${azul}Presiona Enter para salir...${normal}"
