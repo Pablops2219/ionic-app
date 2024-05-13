3be44c6bc13bedf69304f147400d0c77c57aeafd
Joel Pérez Matínez <joelperezmartinez01@gmail.com>
2024-05-13 11:00:12 +0200

    diseño form avehiculo

--- src/app/shared/components/add-update-vehicle/add-update-vehicle.component.ts
+++ src/app/shared/components/add-update-vehicle/add-update-vehicle.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-add-update-vehicle',
@@ -7,8 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUpdateVehicleComponent  implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    uid: new FormControl(''),
+    license_plate: new FormControl('',[Validators.required]),
+    make: new FormControl('',[Validators.required]),
+    year: new FormControl(''),
+    model: new FormControl('',[Validators.required]),
+    color: new FormControl(''),
+    image: new FormControl('',[Validators.required]),
+  })
+  
+    firebaseSvc = inject(FirebaseService);
+    utilsSvc = inject(UtilsService)
+  
+    ngOnInit() {
+    }
+  
+    async submit(){
+      if(this.form.valid){
+  
+        const loading = await this.utilsSvc.loading();
+        await loading.present();
+  
+        this.firebaseSvc.signUp(this.form.value as unknown as User).then( async res => {
 
-  ngOnInit() {}
+          await this.firebaseSvc.updateUser(this.form.value.make)
+          let uid = res.user.uid;
+          this.form.controls.uid.setValue(uid);
 
+        }).catch(error => {
+
+          console.log(error);
+          this.utilsSvc.presentToast({message: error.message, duration: 2500, color: 'tertiary', position:'middle', icon:'alert-circle-outline'})
+
+        }).finally(()=> {
+
+          loading.dismiss();
+
+        }) 
+  
+        
+      }
+    }
+
+
+  dismissModal(){
+    this.utilsSvc.dismissModal();
+  }
 }
